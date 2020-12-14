f283e11b092f417e5a368400b28cbf828d2e5927
Filip A <axelzon8@gmail.com>
2020-12-14 11:49:28 +0100

    Add project files.

---
+++ UnitTest/Chatbot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public static class Chatbot
+    {
+        
+        public static string Reply(string name, int number)
+        {
+            if (name == "")
+            {
+                return "hej";
+            }
+            else if (number >= 1)
+            {
+                return name + " är " + number + " år gammal";
+            }
+            else if (name == "Hjälp mig!")
+            {
+                return "Vad behöver du hjälp med?";
+            }
+            else
+            {
+                return "hej" + name;
+            }
+
+          
+          
+
+
+        
+
+
+        }
+
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+          
+            var answer = Chatbot.Reply("", 0);
+            Assert.AreEqual("hej", answer);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var answer = Chatbot.Reply(" Linus", 0);
+            Assert.AreEqual("hej Linus", answer);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var answer = Chatbot.Reply("Linus", 15);
+            Assert.AreEqual("Linus är 15 år gammal", answer);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var answer = Chatbot.Reply("Hjälp mig!", 0);
+            Assert.AreEqual("Vad behöver du hjälp med?", answer);
+        }
+    }
+}
